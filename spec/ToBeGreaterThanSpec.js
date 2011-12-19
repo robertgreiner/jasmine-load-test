752fe259cded4012ba5358a89fa5eabbf94d7b3d
Robert Greiner <robert@robertgreiner.com>
2011-12-19 10:36:52 -0600

    Added spec files for each type of Jasmine matcher

---
+++ spec/ToBeGreaterThanSpec.js
@@ -0,0 +1,7 @@
+describe("ToEqual() Matcher", function() {
+  describe("run ToEqual() a bunch of times", function() {
+    it("should be equal", function() {
+      expect(isTrue()).toEqual(true);
+    });
+  });
+});
---
+++ src/Utils.js
@@ -0,0 +1,7 @@
+function getInt() {
+	return 5;
+}
+
+function isTrue() {
+    return true;
+}
