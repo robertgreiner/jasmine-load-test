4350549ca49ba648dcc71b8533f196ba7a484ff3
Robert Greiner <robert@robertgreiner.com>
2011-12-19 13:34:53 -0600

    working on matcher templates

--- spec/ToBeLessThanSpec.js
+++ spec/ToBeLessThanSpec.js
@@ -1,7 +1,3 @@
-describe("ToEqual() Matcher", function() {
-  describe("run ToEqual() a bunch of times", function() {
-    it("should be equal", function() {
-      expect(isTrue()).toEqual(true);
-    });
-  });
+describe("toBeLessThan() Matcher", function() {
+
 });
